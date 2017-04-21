f646fbeb59ced513e01169430d2034324226d942
Doug McGeehan <djmvfb@mst.edu>
2017-04-21 14:03:20 -0500

    copy constructor and properly handling an ordered pair as the label of a node

--- test.py
+++ test.py
@@ -10,18 +10,29 @@ class RandomGraph(object):
     edge_line_format = 'e {uid} {vid} {label}'
     available_label = 0
 
-    def __init__(self, num_nodes, num_edges, node_label_generator,
+    def __init__(self, G=None):
+        self.last_node_id = None
+        self.label = RandomGraph.available_label
+        RandomGraph.available_label += 1
+        if G is not None:
+            self.graph = G.copy()
+            self.graph.graph['label'] = self.label
+
+        else:
+            self.graph = networkx.Graph(label=self.label)
+
+    def create(self, num_nodes, num_edges, node_label_generator,
                  edge_label_generator):
         self._assert_legal_parameters(num_nodes, num_edges)
 
-        G = networkx.Graph(label=RandomGraph.available_label)
-        self.label = RandomGraph.available_label
-        RandomGraph.available_label += 1
+        G = self.graph
 
         # create nodes
         for node_id in range(num_nodes):
             G.add_node(node_id, label=node_label_generator())
-        pprint.pprint(G.nodes(data=True))
+            self.last_node_id = node_id
+
+        # pprint.pprint(G.nodes(data=True))
 
         # create edges
         while G.number_of_edges() < num_edges:
@@ -30,16 +41,17 @@ class RandomGraph(object):
                                             replace=False)
             G.add_edge(*node_pair, label=edge_label_generator())
 
-        pprint.pprint(list(G.edges_iter(data=True)))
-        self.graph = G
+        # pprint.pprint(list(G.edges_iter(data=True)))
+        return self
 
     def copy(self):
-        pass
+        cloned = RandomGraph(G=self.graph)
+        return cloned
 
     def translate_by(self, offset):
         pass
 
-    def add_random_node(self, connect_to):
+    def add_random_node(self, connect_to_num_nodes=1):
         pass
 
     def __str__(self):
@@ -50,8 +62,10 @@ class RandomGraph(object):
         #   u_id is the identifier for vertex u,
         #   and label is the label of vertex u
         for vid, v_data in self.graph.nodes(data=True):
+            node_label = '({})'.format(','.join(
+                map(str, v_data['label'])))
             line = RandomGraph.vertex_line_format.format(
-                vid=vid, label=v_data['label'])
+                vid=vid, label=node_label)
             graph_repr_lines.append(line)
 
         # create a string of the form "e u_id v_id label",
@@ -106,11 +120,16 @@ class TestApproximateGraphIsomorphism(unittest.TestCase):
         node_label_generator = lambda: numpy.random.randint(low=0,
                                                             high=100,
                                                             size=(2,))
-        base_subgraph = RandomGraph(num_nodes=4,
-                                    num_edges=5,
-                                    node_label_generator=node_label_generator,
-                                    edge_label_generator=int)
+        base_subgraph = RandomGraph().create(
+            num_nodes=4,
+            num_edges=5,
+            node_label_generator=node_label_generator,
+            edge_label_generator=int)
+
+        graph1 = base_subgraph.copy()
+
         print(base_subgraph)
+        print(graph1)
 
 
 if __name__ == '__main__':
