2a9104d5a004f76f32b4afc601233c4f7a89fedc
Doug McGeehan <djmvfb@mst.edu>
2017-04-21 13:52:16 -0500

    creating a random graph and its string representation

--- test.py
+++ test.py
@@ -6,14 +6,32 @@ numpy.random.seed(0)
 import unittest
 
 class RandomGraph(object):
+    vertex_line_format = 'v {vid} {label}'
+    edge_line_format = 'e {uid} {vid} {label}'
+    available_label = 0
+
     def __init__(self, num_nodes, num_edges, node_label_generator,
                  edge_label_generator):
         self._assert_legal_parameters(num_nodes, num_edges)
 
-        G = networkx.Graph()
+        G = networkx.Graph(label=RandomGraph.available_label)
+        self.label = RandomGraph.available_label
+        RandomGraph.available_label += 1
+
+        # create nodes
         for node_id in range(num_nodes):
             G.add_node(node_id, label=node_label_generator())
         pprint.pprint(G.nodes(data=True))
+
+        # create edges
+        while G.number_of_edges() < num_edges:
+            node_pair = numpy.random.choice(range(num_nodes),
+                                            size=(2,),
+                                            replace=False)
+            G.add_edge(*node_pair, label=edge_label_generator())
+
+        pprint.pprint(list(G.edges_iter(data=True)))
+        self.graph = G
 
     def copy(self):
         pass
@@ -25,7 +43,28 @@ class RandomGraph(object):
         pass
 
     def __str__(self):
-        return 'some graph'
+        graph_repr_lines = ['t # {}'.format(self.label)]
+
+        # create a string of the form "v u_id label"
+        #   where v is the literal character "v",
+        #   u_id is the identifier for vertex u,
+        #   and label is the label of vertex u
+        for vid, v_data in self.graph.nodes(data=True):
+            line = RandomGraph.vertex_line_format.format(
+                vid=vid, label=v_data['label'])
+            graph_repr_lines.append(line)
+
+        # create a string of the form "e u_id v_id label",
+        #   where e is the literal character "e",
+        #   u_id is the identifier for vertex u,
+        #   v_id is the identifier for vertex v,
+        #   and label is the label associated with edge (u, v)
+        for u, v, edge_data in self.graph.edges(data=True):
+            line = RandomGraph.edge_line_format.format(
+                uid=u, vid=v, label=edge_data['label'])
+            graph_repr_lines.append(line)
+
+        return '\n'.join(graph_repr_lines)
 
     def __repr__(self):
         return str(self)
@@ -48,7 +87,14 @@ class GraphDatabaseWriter(object):
         self.graphs = graphs
 
     def write_to(self, path):
-        pass
+        # enumerate each graph and associate an integer identifier with them
+        reprs_for_all_graphs = []
+
+        for g in self.graphs:
+            reprs_for_all_graphs.append(str(g))
+
+        reprs_for_all_graphs.append('t # {}'.format(-1))
+        return '\n'.join(reprs_for_all_graphs)
 
 
 class TestApproximateGraphIsomorphism(unittest.TestCase):
@@ -64,6 +110,7 @@ class TestApproximateGraphIsomorphism(unittest.TestCase):
                                     num_edges=5,
                                     node_label_generator=node_label_generator,
                                     edge_label_generator=int)
+        print(base_subgraph)
 
 
 if __name__ == '__main__':
