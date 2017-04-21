7c8de7d8c322267170282d39ebe59871cb5f9ae6
Doug McGeehan <djmvfb@mst.edu>
2017-04-21 14:25:55 -0500

    adding random nodes to a copy of a base graph

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pprint
-
+import itertools
 import networkx
 import numpy
 numpy.random.seed(0)
@@ -11,26 +11,34 @@ class RandomGraph(object):
     available_label = 0
 
     def __init__(self, G=None):
-        self.last_node_id = None
         self.label = RandomGraph.available_label
         RandomGraph.available_label += 1
         if G is not None:
             self.graph = G.copy()
             self.graph.graph['label'] = self.label
+            self.next_node_id = G.number_of_nodes()
 
         else:
             self.graph = networkx.Graph(label=self.label)
+            self.next_node_id = 0
+
+        self.node_label_generator = None
+        self.edge_label_generator = None
 
     def create(self, num_nodes, num_edges, node_label_generator,
                  edge_label_generator):
         self._assert_legal_parameters(num_nodes, num_edges)
+
+        self.node_label_generator = node_label_generator
+        self.edge_label_generator = edge_label_generator
 
         G = self.graph
 
         # create nodes
         for node_id in range(num_nodes):
             G.add_node(node_id, label=node_label_generator())
-            self.last_node_id = node_id
+
+        self.next_node_id = G.number_of_nodes()
 
         # pprint.pprint(G.nodes(data=True))
 
@@ -46,13 +54,39 @@ class RandomGraph(object):
 
     def copy(self):
         cloned = RandomGraph(G=self.graph)
+        cloned.edge_label_generator = self.edge_label_generator
+        cloned.node_label_generator = self.node_label_generator
         return cloned
 
     def translate_by(self, offset):
         pass
 
-    def add_random_node(self, connect_to_num_nodes=1):
-        pass
+    def add_random_nodes(self, how_many=None, connect_to_num_nodes=None):
+        if how_many is None:
+            how_many = 1
+        if connect_to_num_nodes is None:
+            connect_to_num_nodes = 1
+
+        G = self.graph
+
+        # generate new nodes
+        starting_node_index = self.next_node_id
+        new_node_indices = list(range(starting_node_index, how_many))
+        for node_id in new_node_indices:
+            G.add_node(node_id, label=self.node_label_generator())
+            self.last_node_id = node_id
+
+        self.next_node_id = G.number_of_nodes()
+
+        # connect them up to pre-existing nodes
+        final_edge_count = G.number_of_edges() + connect_to_num_nodes
+        possible_edges = list(itertools.product(G.nodes(),
+                                                new_node_indices))
+        print(possible_edges)
+        while G.number_of_edges() < final_edge_count:
+            to_node = numpy.random.choice(new_node_indices)
+            from_node = numpy.random.choice(G.nodes())
+            G.add_edge(from_node, to_node, label=self.edge_label_generator())
 
     def __str__(self):
         graph_repr_lines = ['t # {}'.format(self.label)]
@@ -114,6 +148,10 @@ class GraphDatabaseWriter(object):
 class TestApproximateGraphIsomorphism(unittest.TestCase):
 
     def setUp(self):
+        self.base_subgraph = []
+        self.exact_subgraph_isomorphisms = []
+
+    def test_exact_subgraph_isomorphism(self):
         pass
 
     def test_approximate_subgraph_isomorphism(self):
@@ -127,6 +165,8 @@ class TestApproximateGraphIsomorphism(unittest.TestCase):
             edge_label_generator=int)
 
         graph1 = base_subgraph.copy()
+        graph1.translate_by(offset=numpy.array([1,0]))
+        graph1.add_random_nodes(how_many=5, connect_to_num_nodes=3)
 
         print(base_subgraph)
         print(graph1)
